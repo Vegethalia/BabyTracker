124dd26d2f02a7aafc72aba416ada9ddc79ecb61
Keseta <pleyhades@gmail.com>
2021-01-10 12:25:29 +0100

    Drawing map in the middle of the page, and cleaning some code and libraries

--- src/Py/TestMapbox.py
+++ src/Py/TestMapbox.py
@@ -8,8 +8,6 @@ import DbAccess as db
 import Params
 import plotly.express as px
 from datetime import datetime
-import geocoder
-from flask import request
 import plotly.graph_objects as go
 from dash.dependencies import ClientsideFunction, Input, Output #per fer funcions js a client
 
@@ -55,6 +53,7 @@ cap=dbc.Jumbotron(fluid=True, className="pt-2 pb-2", children=[
 app.layout=html.Div(children=[
 	cap,
     dbc.Row(children=[
+            dbc.Col(width=1),
             dbc.Col(className="aborder border-primary", width=4,  children=[
                  html.Div([
                     'Tipus de Mapa',
@@ -73,10 +72,12 @@ app.layout=html.Div(children=[
                     html.Span(id="idLocation", style={"vertical-align": "middle"}),
                     html.Span("hola Bola", id="idOutputClient", style={"vertical-align": "middle"})
                 ]) 
-          ])
+          ]),
+          dbc.Col(width=1),
         ]),
 
     dbc.Row(children=[
+        dbc.Col(width=1),
         dbc.Col(width=10, children=[
             html.Div([
                    'Seleccionar últims minuts',
@@ -89,16 +90,19 @@ app.layout=html.Div(children=[
                         value=5                        
                     )
                 ],style={'height':75})
-            ])
+            ]),
+        dbc.Col(width=1),
         ]),
     dbc.Row(children=[
+        dbc.Col(width=1),
         dbc.Col(className="aborder border-primary", width=10, children=[
             dcc.Graph(id='idBabyShowMap')
-        ])
+        ]),
+        dbc.Col(width=1),
     ])
 ])
 
-#dibuixem tot el mapa amb el que es necessita. tipus de mapa, loc de l'usuari i llista de punts
+#dibuixem tot el mapa amb el que es necessita. tipus de mapa, loc de l'usuari i llista de punts del esp32
 @app.callback(
     Output("idLocation", "children"), 
     Output("idBabyShowMap", "figure"),
@@ -119,7 +123,7 @@ def update_MapTotal(TipusMap, TempsSelected, geolocationUser):
         text=dfSelected.LocDate,
         marker = {'size': 10, 'color':'fuchsia'},
         name='Arlet&Erola'))
-        
+     #si tenim la geocalització de l'usuari, la pintem
     myloc=geolocationUser.split(",")
     if (len(myloc)==2):   
         fig.add_trace(go.Scattermapbox(
@@ -158,22 +162,6 @@ def update_MapTotal(TipusMap, TempsSelected, geolocationUser):
     #    title_text='Cercador de Babys')
     return  f" --Slider SELECT: {TempsSelected} -- GEOLOCATION: {myloc} --",fig
 
-
-#Modifiquem el tipus de mapa segons el que hagi escollit l'usuari al dropdownlist
-#@app.callback(
-#    Output("idBabyShowMap","figure"), Input("idTipusMap","value")
-#)
-#def update_TipusMap(TipusMap):
-# fig.update_layout(
-#    margin ={'l':0,'t':0,'b':0,'r':0},
-#    height=750,
-#    mapbox = {
-#        'center': {'lon':(MIN_LONGITUDE+MAX_LONGITUDE)/2, 'lat': (MIN_LATITUDE+MAX_LATITUDE)/2},
-#        'accesstoken':MapBoxToken,
-#        'style': TipusMap,
-#        'zoom': 12},
-#    title_text='Cercador de Babys')
-# return(fig)
 
 #funció que s'executa en client al navegador i obté les coordenades gps de l'usuari
 app.clientside_callback(
